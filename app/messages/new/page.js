fe70da5f9267c61c152f2b46ff8a71d602330a0f
YusufAbic <yusuf.abic@outlook.com>
2024-10-24 15:19:03 +0300

    configuring caching

--- app/messages/new/page.js
+++ app/messages/new/page.js
@@ -1,14 +1,16 @@
-import { redirect } from 'next/navigation';
+import { redirect } from "next/navigation";
 
-import { addMessage } from '@/lib/messages';
+import { addMessage } from "@/lib/messages";
+import { revalidatePath, revalidateTag } from "next/cache";
 
 export default function NewMessagePage() {
   async function createMessage(formData) {
-    'use server';
-
-    const message = formData.get('message');
+    "use server";
+    const message = formData.get("message");
     addMessage(message);
-    redirect('/messages');
+    revalidateTag("msg");
+    // revalidatePath("/messages");
+    redirect("/messages");
   }
 
   return (
--- app/messages/page.js
+++ app/messages/page.js
@@ -1,13 +1,16 @@
-import Messages from '@/components/messages';
+import Messages from "@/components/messages";
+import { getMessages } from "../../lib/messages";
+import { unstable_noStore } from "next/cache";
+// export const revalidate = 5;
+// export const dynamic = "force-dynamic";
 
 export default async function MessagesPage() {
-  const response = await fetch('http://localhost:8080/messages', {
-    headers: {
-      'X-ID': 'page',
-    },
-  });
-  const messages = await response.json();
-
+  // unstable_noStore();
+  // const response = await fetch("http://localhost:8080/messages", {
+  //   next: { tags: ["msg"] },
+  // });
+  // const messages = await response.json();
+  const messages = await getMessages();
   if (!messages || messages.length === 0) {
     return <p>No messages found</p>;
   }
